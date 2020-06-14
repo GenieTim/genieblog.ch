81f87284a36b06712eea769749957d827f35a0de
Tim Bernhard <tim@bernhard-webstudio.ch>
2020-06-14 11:27:59 +0200

    Update CopyWebpackPlugin config

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,9 +23,11 @@ mix.webpackConfig({
             'source/**/*.php',
             'source/**/*.scss',
         ]),
-        new CopyWebpackPlugin([
-            { from: 'source/assets/build/images', to: 'images' }
-        ]),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'source/assets/build/images', to: 'images' }
+            ]
+        }),
         new GenerateSW({
             // these options encourage the ServiceWorkers to get in there fast
             // and not allow any straggling "old" SWs to hang around
