28fccb1b9fd370557e62d2346f7d7e012a479e82
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-01-26 17:59:56 +0100

    Upgrade CSS & Scripts To Match Dependency Versions
    Thanks to https://github.com/tighten/jigsaw-blog-template/pull/78 for
    the relevant change: in webpack.mix.js: .vue()

--- webpack.mix.js
+++ webpack.mix.js
@@ -30,6 +30,7 @@ mix.webpackConfig({
 });
 
 mix.js('source/_assets/js/main.js', 'js')
+    .vue()
     .sass('source/_assets/sass/main.scss', 'css/main.css')
     .jigsaw({
         watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.scss'],
