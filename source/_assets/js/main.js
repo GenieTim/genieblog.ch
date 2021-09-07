9b4286ec7d2aff0a71f882c685cc688eff209426
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-09-07 11:02:39 +0200

    Revert to using Vue 2 syntax

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,52 +1,59 @@
 /**
  * Service Worker setup for offline support
  */
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(reg => {
-                console.log('Registration succeeded. Scope is ' + reg.scope);
-            })
-            .catch(registrationError => {
-                console.log('SW registration failed: ', registrationError);
-            });
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => {
+        console.log("Registration succeeded. Scope is " + reg.scope);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
 }
 
 /**
  * Instant click for perceived performance improvement
  */
-const InstantClick = require('instantclick');
+const InstantClick = require("instantclick");
 
 /**
  * Search functionality
  */
-window.axios = require('axios');
-import Search from './components/Search.vue';
-import { createApp } from 'vue';
+window.axios = require("axios");
+// import { createApp } from 'vue';
+import Vue from "vue";
+import Search from "./components/Search.vue";
 
 function initializeVue() {
-    // console.log(Vue);
-    createApp({
-        components: {
-            Search,
-        },
-    }).mount('#vue-search');
-};
+  // console.log(Vue);
+  // createApp({
+  //     components: {
+  //         Search,
+  //     },
+  // }).mount('#vue-search');
+  new Vue({
+    components: {
+      Search,
+    },
+  }).$mount("#vue-search");
+}
 
 /**
  * Register events of InstantClick
  */
 
-InstantClick.on('change', function () {
-    try {
-        ga('send', 'pageview', location.pathname + location.search);
-    } catch (e) {
-        // eighter adblock/tracker blocking or development site
-        console.log(e);
-    }
-    // (re)-initialize search
-    initializeVue();
+InstantClick.on("change", function() {
+  try {
+    ga("send", "pageview", location.pathname + location.search);
+  } catch (e) {
+    // eighter adblock/tracker blocking or development site
+    console.log(e);
+  }
+  // (re)-initialize search
+  initializeVue();
 });
 // finally, initialize
 InstantClick.init();
@@ -54,7 +61,7 @@ InstantClick.init();
 /**
  * Math. Possibly better to do serverside?
  */
-require('./mathjax-config');
+require("./mathjax-config");
 // const MathJax = require('mathjax/es5/tex-svg')
 // InstantClick.on('change', () => {
 //     MathJax.typeset()
